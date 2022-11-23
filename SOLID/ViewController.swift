10f4b77600d749aa9712d8f45815351454b6c151
Francisco Javier Saldivar Rubio <francisco.saldivar@ua.la>
2022-11-23 09:54:04 -0600

    Initial Commit

---
+++ SOLID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID
+//
+//  Created by Francisco J. Saldivar on 23/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
